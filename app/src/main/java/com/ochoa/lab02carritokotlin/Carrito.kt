e0941a72109f1abca7d0b9ed85add35da14919f2
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-08-26 11:44:11 -0500

    Agrega reporte de detalle con columnas alineadas

--- app/src/main/java/com/ochoa/lab02carritokotlin/Carrito.kt
+++ app/src/main/java/com/ochoa/lab02carritokotlin/Carrito.kt
@@ -6,6 +6,17 @@ data class Producto(
     var cantidad: Int,
 )
 
+fun mostrarDetalle(productos: List<Producto>) {
+    println("-----DETALLE DEL CARRITO-----")
+    var i = 1
+    for (p in productos){
+        val importe = p.precio*p.cantidad
+        println(String.format("%d. %-20s x%d S/ %8.2f",
+            i, p.nombre, p.cantidad, importe))
+        i++
+    }
+    println("------------------------------")
+}
 fun calcularSubtotal(productos: List<Producto>): Double {
     var subtotal = 0.0
     for (p in productos) {
@@ -17,13 +28,13 @@ fun calcularSubtotal(productos: List<Producto>): Double {
 
 fun calcularIGV(subtotal: Double): Double {
     val igv = subtotal * 0.18
-    println("IGV (18%): $igv")
+    println(String.format("%-9s: S/ %.2f","IGV (18%)", igv))
     return igv
 }
 
 fun calcularTotal(subtotal: Double, igv: Double): Double {
     val total = subtotal + igv
-    println("TOTAL: $total")
+    println(String.format("%-9s: S/ %.2f", "TOTAL",total))
     return total
 }
 
@@ -35,20 +46,24 @@ fun main() {
     val nombreCliente = "Yamil Ochoa"
     val carrito = mutableListOf<Producto>()
     println("Cliente: $nombreCliente")
-    println()
 
     carrito.add(Producto("Laptop HP", 2500.0, 1))
     carrito.add(Producto("Mouse Logitech", 45.5, 2))
     carrito.add(Producto("Monitor Xiaomi 45",500.0,4 ))
     carrito.add(Producto("Arduino UNO ", 50.0,4))
+    println()
 
     for (producto in carrito) {
         println("Producto agregado: ${producto.nombre}")
     }
-
     println()
+    mostrarDetalle(carrito)
+    println()
+    println("Cantidad de productos: ${carrito.size}")
+    println()
+
     val subtotal = calcularSubtotal(carrito)
-    println("Subtotal: $subtotal")
+    println(String.format("%-9s: S/ %.2f", "Subtotal", subtotal))
 
     val igv = calcularIGV(subtotal)
     calcularTotal(subtotal, igv)
