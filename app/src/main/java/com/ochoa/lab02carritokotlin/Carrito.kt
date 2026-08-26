18d91b592392d656c6b617233509be807818c571
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-08-25 22:13:42 -0500

    Proyecto inicial con menu del carrito

---
+++ app/src/main/java/com/ochoa/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,10 @@
+package com.ochoa.lab02carritokotlin
+
+class Carrito {
+}
+
+fun main() {
+    println("====================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("====================================")
+}
