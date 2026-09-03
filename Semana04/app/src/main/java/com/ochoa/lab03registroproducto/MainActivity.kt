6827f968dc552a5583133b8944d417ff03ea2350
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-09-03 00:07:42 -0500

    Agregar encabezado con jerarquia tipografica

--- Semana04/app/src/main/java/com/ochoa/lab03registroproducto/MainActivity.kt
+++ Semana04/app/src/main/java/com/ochoa/lab03registroproducto/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ochoa.lab03registroproducto.ui.theme.Lab03RegistroProductoTheme
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab03RegistroProductoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    PantallaRegistro(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +33,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun PantallaRegistro(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Nuevo producto",
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Text(
+            text = "Completa los datos y presiona Agregar",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.outline
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = "Nombre del producto")
+        Text(text = "Precio (S/)")
+        Text(text = "Cantidad")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PantallaRegistroPreview() {
     Lab03RegistroProductoTheme {
-        Greeting("Android")
+        PantallaRegistro()
     }
 }
