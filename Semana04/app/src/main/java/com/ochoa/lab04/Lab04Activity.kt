4a9214c51027fba61e5f2e7bc555803fb2018636
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-09-13 19:43:06 -0500

    Se agrego codigo base de la actividad

---
+++ Semana04/app/src/main/java/com/ochoa/lab04/Lab04Activity.kt
@@ -0,0 +1,33 @@
+package com.ochoa.lab04
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+data class Tarea(
+    val id: Int,
+    val nombre: String,
+    val completada: Boolean = false
+)
+
+class Lab04Activity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaterialTheme {
+                PantallaTareas()
+            }
+        }
+    }
+}
+
