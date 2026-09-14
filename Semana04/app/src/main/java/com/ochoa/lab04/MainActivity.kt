28d193f8f06174182219d1bb6b9284e1d1b1aa1a
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-09-13 19:02:03 -0500

    Se creo el composable Temperature

---
+++ Semana04/app/src/main/java/com/ochoa/lab04/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.ochoa.lab04
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxSize
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface( modifier = Modifier.fillMaxSize()) { }
+        }
+    }
+}
+
+@Composable
+fun TemperatureDisplay(modifier: Modifier = Modifier) {
+
+}
+
