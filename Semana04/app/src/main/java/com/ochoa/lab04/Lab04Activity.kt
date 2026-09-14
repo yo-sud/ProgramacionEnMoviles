e0b73d0e561d8ebd7b63936d6ee45e4953e14a1b
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-09-13 20:38:12 -0500

    Se agrega tarjeta de cada tarea

--- Semana04/app/src/main/java/com/ochoa/lab04/Lab04Activity.kt
+++ Semana04/app/src/main/java/com/ochoa/lab04/Lab04Activity.kt
@@ -12,6 +12,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 
 data class Tarea(
     val id: Int,
@@ -26,6 +32,49 @@ class Lab04Activity : ComponentActivity() {
         setContent {
             MaterialTheme {
                 PantallaTareas()
+            }
+        }
+    }
+}
+
+@Composable
+fun ItemTarea(
+    tarea: Tarea,
+    onEliminar: () ->Unit,
+    onCambiarEstado: (Boolean) -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                modifier = Modifier.weight(1f)
+            ) {
+                Checkbox(
+                    checked = tarea.completada,
+                    onCheckedChange = {
+                        onCambiarEstado(it)
+                    }
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Text(
+                    text = tarea.nombre,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(top = 12.dp)
+                )
+            }
+
+            Button(onClick = onEliminar) {
+                Text("Eliminar")
             }
         }
     }
