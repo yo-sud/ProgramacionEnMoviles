dd3c11d55aa53b06e8fcb58129c0598d446d6972
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-09-17 17:32:39 -0500

    Proyecto Inicial

--- Lab04CarritoTecsup/app/src/main/java/com/ochoa/lab04carritoOchoa/MainActivity.kt
+++ Lab04CarritoTecsup/app/src/main/java/com/ochoa/lab04carritoOchoa/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab04CarritoTecsupTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
