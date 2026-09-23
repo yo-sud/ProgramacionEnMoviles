a95b6f92e73f155ea1fbd1c5fa58115d0cb5a55e
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-09-22 20:49:17 -0500

    Crea Composable con rutas de las pantallas

--- Semana05/app/src/main/java/com/ochoa/lab05/MainActivity.kt
+++ Semana05/app/src/main/java/com/ochoa/lab05/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.ochoa.lab05.navigation.AppNavigation
+import com.ochoa.lab05.navigation.Screen
+import com.ochoa.lab05.screens.DetailScreen
+import com.ochoa.lab05.screens.HomeScreen
+import com.ochoa.lab05.screens.ListScreen
+import com.ochoa.lab05.screens.ProfileScreen
 import com.ochoa.lab05.ui.theme.Lab05Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +24,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Lab05Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppNavigation()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppNavigation() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab05Theme {
-        Greeting("Android")
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+
+        composable(Screen.List.route) {
+            ListScreen(navController)
+        }
+
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument(name = "itemId") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt(key = "itemId") ?: 0
+            DetailScreen(navController, itemId)
+        }
     }
 }
