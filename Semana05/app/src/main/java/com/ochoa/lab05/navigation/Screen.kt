ea6666e7230f674b5d3211cfce9f056b16e3349d
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-09-22 20:41:22 -0500

    Implementando rutas con sealed class

--- Semana05/app/src/main/java/com/ochoa/lab05/navigation/Screen.kt
+++ Semana05/app/src/main/java/com/ochoa/lab05/navigation/Screen.kt
@@ -1,4 +1,16 @@
 package com.ochoa.lab05.navigation
 
-class Screen {
+sealed class Screen(val route: String) {
+
+    object Home : Screen("home")
+
+    object List : Screen("list")
+
+    object Profile : Screen("profile")
+
+
+    object Detail : Screen("detail/{itemId}") {
+
+        fun createRoute(itemId: Int): String = "detail/$itemId"
+    }
 }
