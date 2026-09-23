bae5649ee8823e81d52fc420ba1ebc4afe393865
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-09-22 20:55:01 -0500

    Agrega estructura simple del home

--- Semana05/app/src/main/java/com/ochoa/lab05/screens/HomeScreen.kt
+++ Semana05/app/src/main/java/com/ochoa/lab05/screens/HomeScreen.kt
@@ -1,2 +1,40 @@
 package com.ochoa.lab05.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.ochoa.lab05.navigation.Screen
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Pantalla Tecsup",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(
+            onClick = { navController.navigate(Screen.List.route) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Ver lista de elementos")
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        OutlinedButton(
+            onClick = { navController.navigate(Screen.Profile.route) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Mi perfil")
+        }
+    }
+}
