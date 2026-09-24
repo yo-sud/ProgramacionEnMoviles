9c23c261d0d5fac74420239181f4d6104f88b181
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-09-23 19:06:12 -0500

    COnfigurar navegacion secuencial (NavHost) con ruta Inicio a Detalle

--- Semana05/Proyecto Scafold/app/build.gradle.kts
+++ Semana05/Proyecto Scafold/app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-compose:2.8.0")
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
--- Semana05/Proyecto Scafold/app/src/main/java/com/ochoa/tecsupfit/MainActivity.kt
+++ Semana05/Proyecto Scafold/app/src/main/java/com/ochoa/tecsupfit/MainActivity.kt
@@ -3,45 +3,23 @@ package com.ochoa.tecsupfit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.ochoa.tecsupfit.navigation.NavGraph
 import com.ochoa.tecsupfit.ui.theme.TecsupfitTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TecsupfitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                NavGraph(
+                    navController = navController,
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TecsupfitTheme {
-        Greeting("Android")
     }
 }
