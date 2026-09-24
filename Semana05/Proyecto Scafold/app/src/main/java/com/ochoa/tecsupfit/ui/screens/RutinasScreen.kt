2a9efb9c10c2a5e285f0bfae271290eb9d5a881d
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-09-24 10:29:52 -0500

    Crea una pantalla simple para seccion rutinas

---
+++ Semana05/Proyecto Scafold/app/src/main/java/com/ochoa/tecsupfit/ui/screens/RutinasScreen.kt
@@ -0,0 +1,23 @@
+package com.ochoa.tecsupfit.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RutinasScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize().padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Próximamente: rutinas personalizadas",
+            style = MaterialTheme.typography.titleMedium
+        )
+    }
+}
