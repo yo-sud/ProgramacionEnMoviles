35a43fa1203d33185b07babb0003f01e2c8c302d
yo-sud <yamil.ochoa@tecsup.edu.pe>
2026-09-24 10:28:11 -0500

    Agrega un bottoomBar con icono y etiqueta

--- Semana05/Proyecto Scafold/app/src/main/java/com/ochoa/tecsupfit/navigation/Pantalla.kt
+++ Semana05/Proyecto Scafold/app/src/main/java/com/ochoa/tecsupfit/navigation/Pantalla.kt
@@ -1,13 +1,29 @@
 package com.ochoa.tecsupfit.navigation
 
-sealed class Pantalla(val ruta: String) {
-    object Inicio : Pantalla("inicio")
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.FitnessCenter
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class Pantalla(
+    val ruta: String,
+    val etiqueta: String? = null,
+    val icono: ImageVector? = null
+) {
+    object Inicio : Pantalla("inicio", "Inicio", Icons.Filled.Home)
     object Detalle : Pantalla("detalle/{claseId}") {
         fun crearRuta(claseId: Int) = "detalle/$claseId"
     }
     object Confirmacion : Pantalla("confirmacion/{claseId}/{horario}") {
         fun crearRuta(claseId: Int, horario: String) = "confirmacion/$claseId/$horario"
     }
-    object Reservas : Pantalla("reservas")
-    object Perfil : Pantalla("perfil")
+    object Reservas : Pantalla("reservas", "Reservas", Icons.Filled.DateRange)
+    object Rutinas : Pantalla("rutinas", "Rutinas", Icons.Filled.FitnessCenter)
+    object Perfil : Pantalla("perfil", "Perfil", Icons.Filled.Person)
+
+    companion object {
+        val itemsBottomBar = listOf(Inicio, Reservas, Rutinas, Perfil)
+    }
 }
